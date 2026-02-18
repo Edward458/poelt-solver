dc7801662df8adb4ca00eb747316cb24e279c909
manny <callofportal12@gmail.com>
2026-02-17 19:46:08 -0500

    cleaning

--- test.py
+++ test.py
@@ -1,10 +1,21 @@
 from bs4 import BeautifulSoup
+from get_roster import get_roster
+import requests
 
-with open('teams-list.html', 'r', encoding='utf-8') as file:
-    html = file.read()
+url = 'https://www.basketball-reference.com/teams/BOS/2026.html'
+response = requests.get(url)
+html = response.text
 
 soup = BeautifulSoup(html,'html.parser')
 
-
+roster = get_roster(soup)
     
+names = roster['name']
+positions = roster['position']
+numbers = roster['number']
+ages = roster['age']
+heights = roster['height']
+
+for i in range(len(roster['name'])):
+
 
